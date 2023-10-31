fe422bc6ad6d4853185732f0d116d13073fbc7ed
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-10-31 05:10:34 +0000

    about

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about-container">
+    <h1>About Us</h1>
+    <p>This is the about page of our website. Learn more about our company and our mission.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
+
+<style scoped>
+.about-container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
