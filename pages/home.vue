801e6e2d60967d24c9ade0d2ddcd520a65901899
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-11-14 05:35:10 +0000

    logo

--- pages/home.vue
+++ pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
-    <img src="images/エントランス.png" alt="エントランス" width="1080" height="720">
-    <h1>京都にIT・デザインに特化した専門学校誕生</h1>
+    <img src="images/barilogo.png" alt="ロゴ" style="width: 300px; height: auto;">
+    
   </div>
 </template>
   
