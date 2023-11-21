781bd7cd420c6fac10a6ab4b51b761d6106030eb
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-11-21 04:43:09 +0000

    写真変更

--- pages/home.vue
+++ pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
     <img src="images/barilogo.png" alt="ロゴ" style="width: 300px; height: auto;">
-    <img src="images/ramen.jpeg" alt="ラーメン" style="width: 700px; height: auto;">
+    <img src="images/ramen2.5.jpg" alt="ラーメン" style="width: 700px; height: auto;">
     
   </div>
 </template>
