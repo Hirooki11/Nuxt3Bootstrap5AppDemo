1299e1a2d8ec4c0dc446aa8a17627f2bf8927565
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-05 07:15:07 +0000

    削除

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,40 +1,13 @@
 <template>
-  <footer class="app-footer mt-auto py-3 bg-light">
-    <div class="Footer-container">
-      <div class="row">
-        <div class="col-6">
-          <h5>フォローする</h5>
+  <div>
+    <footer class="fixed-bottom bg-light py-3 mt-auto">
+      <div class="Footer-container">
+        <h5>フォローする</h5>
           <p class="follow-link">
             Twitter: <a href="https://twitter.com/kyoto_tech_" target="_blank">@kyoto_tech_</a><br>
             Instagram: <a href="https://Instagram.com/kyoto_tech_insta" target="_blank">@kyoto_tech_insta</a>
           </p>
-        </div>
       </div>
-      <hr>
-      <p class="text-muted">© 2023 福元グループ</p>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
-  
-<style scoped>
-/* ここに必要なスタイルを追加します */
-.app-footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 60px;
-  line-height: 60px;
-  background-color: #f5f5f5;
-}
-
-/* p.follow-linkのマージンとパディングを0に設定 */
-p.follow-link {
-  margin: 0;
-  padding: 0;
-}
-/* a要素のデフォルトスタイルを上書き */
-p.follow-link a {
-  text-decoration: none; /* リンクの下線をなくす */
-  color: black; /* リンクの色を黒に設定 */
-}
-</style>
--- components/Bariuma.vue
+++ components/Bariuma.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="Bariuma-container">
+    <div class="Bariuma-container mb-5">
         <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;">
 
         <span style="display: inline-block; width: 20px;"></span>
         
         <img src="images/menu.jpeg" alt="メニュー" style="width: 700px; height: auto;">
+        
     </div>
 </template>
