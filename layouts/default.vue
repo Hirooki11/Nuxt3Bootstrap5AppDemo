f01af8de980c0522c1d151c50db82ce0f21abb7b
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-05 06:47:00 +0000

    CSS削除

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,9 @@
 <template>
   <div>
     <AppHeader />
-    <main>
+    <main class="container py-4">
       <slot />
     </main>
     <AppFooter />
   </div>
 </template>
-
-<script>
-</script>
-
-<style scoped>
-
-body {
-  font-family: 'Arial', sans-serif;
-  margin: 0;
-  padding: 0;
-}
-
-main {
-  padding: 20px;
-}
-
-</style>
