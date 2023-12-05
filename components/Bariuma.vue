db56da3f386e716d2e5b6aaef13947dd69a9ad7b
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-05 05:30:29 +0000

    home

--- components/Bariuma.vue
+++ components/Bariuma.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="Bariuma-container">
+        <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;">
 
+        <span style="display: inline-block; width: 20px;"></span>
+        
         <img src="images/menu.jpeg" alt="メニュー" style="width: 700px; height: auto;">
-
     </div>
 </template>
--- components/Home.vue
+++ components/Home.vue
@@ -1,18 +1,17 @@
 <template>
-      <div class="home-container d-flex justify-content-center align-items-center" style="height: 100vh;">
-      <img src="images/ramen2.5.jpg" alt="ラーメン" style="width: 700px; height: auto;">
-      <h1>この記事ではラーメンが大好きな私が今まで食べてきたラーメンを紹介しようと思います</h1>
-    </div>
+<div class="home-container">
+      
+   <h1>この記事ではラーメンが大好きな私が今まで食べてきたラーメンを紹介しようと思います</h1>
+  <div class="barikoku">
+    <img src="images/ramen2.5.jpg" alt="ラーメン" style="width: 700px; height: auto;">
+  </div>
+</div>
   </template>
     
   <script>
   </script>
     
  <style scoped>
-.home-container {
-  display: flex;
-  justify-content: center; /* 水平方向の中央揃え */
-  align-items: center; /* 垂直方向の中央揃え */
-  height: 100vh; /* 画面の高さいっぱいに表示するための設定 */
-}
+
+
 </style>
