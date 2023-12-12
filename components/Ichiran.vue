4c6dde6a06b391a4f4bdcead8336200fa3756883
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-12 03:19:56 +0000

    ichiran

--- components/Ichiran.vue
+++ components/Ichiran.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="Ichiran-container">
-       <h2>一蘭</h2>
-       <img src="images/ichiran.jpeg" alt="一蘭" style="width: 700px; height: auto;">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>一蘭</h2>
+                <img src="images/ichiran.jpeg" alt="一蘭" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>一蘭</h3>
+                <h4>〒600-8006
+                    京都府京都市下京区立売中之町102-3 ミューズ389京都ビル2F
+                    075-211-7755
+                    10:00～22:00
+                </h4>
+            </div>
+        </div>
     </div>
 </template>
