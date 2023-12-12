d70b4bef8ce582c195b371ef0a48adf9e7d657a3
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-12 05:12:59 +0000

    zuneo

--- components/Kairikiya.vue
+++ components/Kairikiya.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="Kairikiya-container">
-        <h2>魁力屋</h2>
-        <img src="images/kairikiya.jpeg" alt="魁力屋" style="width: 700px; height: auto;">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>魁力屋</h2>
+                <img src="images/kairikiya.jpeg" alt="魁力屋" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>京都北白川ラーメン魁力屋 丹波口店</h3>
+                <h4>〒600-8812<br>
+                    京都府京都市下京区中堂寺北町15-1<br>
+                    075-874-4499<br>
+                    11:00-24:00（ラストオーダー23:45)
+                </h4>
+            </div>
+        </div>
     </div>
 </template>
