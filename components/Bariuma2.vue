7861bc54057cfcd7d5977a0d2aa8b46f8d06d0af
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-12 04:44:51 +0000

    ばり馬

--- components/Bariuma2.vue
+++ components/Bariuma2.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="Bariuma2-container">
-        <h2>ばり馬</h2>
-        <img src="images/bariuma2.jpeg" alt="ばり馬" style="width: 700px; height: auto;">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>ばり馬</h2>
+                <img src="images/bariuma2.jpeg" alt="ばり馬" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>熟成麺屋　神来　西院本店</h3>
+                <h4>〒649-6231<br>
+                    和歌山県岩出市川尻17-8<br>
+                    0736-69-5850<br>
+                    11:00～24:00(LO.23:30)
+                </h4>
+            </div>
+        </div>
     </div>
 </template>
