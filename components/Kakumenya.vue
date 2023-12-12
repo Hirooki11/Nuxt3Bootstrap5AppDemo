62ab0c3730b64fc366e28e06ea1f3ffd6bd5979f
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-12 05:22:09 +0000

    kakumen

--- components/Kakumenya.vue
+++ components/Kakumenya.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="Kakumenya-container">
-        <h2>革麺家</h2>
-        <img src="images/kakumenya.jpeg" alt="革麺家" style="width: 700px; height: auto;">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>革麺家</h2>
+                <img src="images/kakumenya.jpeg" alt="革麺家" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>横浜家系ラーメン 革麺家 岩出店</h3>
+                <h4>〒649-6235<br>
+                    和歌山県岩出市西野211-1 1F<br>
+                    0736-67-7030<br>
+                    11:00～24:30(L.O.24:00)
+                </h4>
+            </div>
+        </div>
     </div>
 </template>
