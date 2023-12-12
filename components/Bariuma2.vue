ce4d24cb803788b5281c12c993d60ac1205b958e
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-12 05:02:06 +0000

    一風

--- components/Bariuma2.vue
+++ components/Bariuma2.vue
@@ -6,7 +6,7 @@
                 <img src="images/bariuma2.jpeg" alt="ばり馬" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
-                <h3>熟成麺屋　神来　西院本店</h3>
+                <h3>ばり馬和歌山岩出店</h3>
                 <h4>〒649-6231<br>
                     和歌山県岩出市川尻17-8<br>
                     0736-69-5850<br>
--- components/Ippudo.vue
+++ components/Ippudo.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="Ippudo-container">
-        <h2>一風堂</h2>
-        <img src="images/ippudo.jpeg" alt="一風堂" style="width: 700px; height: auto;">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>一風堂</h2>
+                <img src="images/ippudo.jpeg" alt="一風堂" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>一風堂 京都ポルタ店</h3>
+                <h4>〒600-8216<br>
+                    京都府 京都市 下京区 烏丸通塩小路下る東塩小路町902<br>
+                    京都ポルタ 「東エリア」内<br>
+                    075-708-5300<br>
+                    11:00 ~ 22:00
+                </h4>
+            </div>
+        </div>
     </div>
 </template>
