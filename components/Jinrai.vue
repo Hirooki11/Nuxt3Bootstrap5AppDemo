00cdeec7a209f8505f5e81b02d2a062d358554e2
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-12 04:37:09 +0000

    br

--- components/Jinrai.vue
+++ components/Jinrai.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="Jinrai-container">
-        <h2>神来</h2>
-        <img src="images/jinrai.jpeg" alt="神来" style="width: 700px; height: auto;">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>神来</h2>
+                <img src="images/ichiran.jpeg" alt="神来" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>熟成麺屋　神来　西院本店</h3>
+                <h4>〒604-8855<br>
+                    京都府京都市中京区壬生淵田町1<br>
+                    075-315-9588<br>
+                    【月曜日〜木曜日】<br>
+                    11:30〜24:00（L.O.23:30）<br>
+                    【金曜日】<br>
+                    11:30〜24:30（L.O.24:00）<br>
+                    【土曜日】<br>
+                    11:00〜24:30（L.O.24:00）<br>
+                    【日曜日】<br>
+                    11:00〜24:00（L.O.23:30）<br>
+                </h4>
+            </div>
+        </div>
     </div>
 </template>
