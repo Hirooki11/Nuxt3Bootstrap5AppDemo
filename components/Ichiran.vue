f81e3fd389fe5d5afa6dec1251a91edcef3c0ab8
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-19 04:42:44 +0000

    itiran

--- components/Ichiran.vue
+++ components/Ichiran.vue
@@ -6,7 +6,7 @@
                 <img src="images/ichiran.jpeg" alt="一蘭" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
-                <h3>一蘭</h3>
+                <h3>一蘭 京都烏丸店</h3>
                 <h4>〒600-8006<br>
                     京都府京都市下京区立売中之町102-3 ミューズ389京都ビル2F<br>
                     075-211-7755<br>
