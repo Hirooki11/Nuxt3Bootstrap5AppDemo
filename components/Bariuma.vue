cae2f8a4191138f84dbedd43f7b51e92dcc6137f
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2023-12-19 04:56:25 +0000

    mb-5

--- components/Bariuma.vue
+++ components/Bariuma.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="Bariuma-container mb-5">
-        <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;">
-
-        <span style="display: inline-block; width: 20px;"></span>
+        <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;" class="mb-5">
         
         <img src="images/menu.jpeg" alt="メニュー" style="width: 700px; height: auto;">
         
