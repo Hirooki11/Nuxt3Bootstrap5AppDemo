144cf2fd00dabea9ff81556ba0ba28273b6d68cc
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-01-16 05:30:56 +0000

    h4

--- pages/recommendation.vue
+++ pages/recommendation.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <div style="font-size: 20px;">
     <h1>私のおすすめは<span class="text-danger strong">ばり馬</span>です</h1>
-    <p>これは僕の地元<strong>和歌山県</strong>にあるお店です</p>
+    <h4>これは僕の地元<strong>和歌山県</strong>にあるお店です</h4>
     <Bariuma />
   </div>
 </template>
