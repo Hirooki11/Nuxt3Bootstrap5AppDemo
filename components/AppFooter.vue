4204669f4b0d78e6ea553c67966e13556f5147e8
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-01-19 17:28:58 +0000

    YouTube

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,6 +2,7 @@
   <footer class="position-fixed bottom-0 end-0 bg-light py-3">
     <div class="container">
       <p class="follow-link">
+        YouTube: <a href="https://www.youtube.com/@user-xu5ms2gq4w" target="_blank">@user-xu5ms2gq4w</a><br>
         Twitter: <a href="https://twitter.com/kyoto_tech_" target="_blank">@kyoto_tech_</a><br>
         Instagram: <a href="https://instagram.com/kyoto_tech_insta" target="_blank">@kyoto_tech_insta</a>
       </p>
