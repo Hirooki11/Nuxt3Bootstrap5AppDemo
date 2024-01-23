e0d9383257a2bab682fa940e27b7fe1bf2ad9308
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-01-23 06:20:02 +0000

    横に並べた

--- components/Bariuma.vue
+++ components/Bariuma.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="Bariuma-container mb-5">
         <div class="row">
-                <div class="col-md-6">
-                    <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;" class="mb-5">
-                    <div v-for="ba in bariuma.adress">
-                        {{ ba.text }}
-                    </div>
-                <div class="col-md-6">
-                    <img src="images/menu.jpeg" alt="メニュー" style="width: 700px; height: auto;">
+            <div class="col-md-6 mb-5">
+                <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;">
+                <div v-for="ba in bariuma.adress">
+                    {{ ba.text }}
                 </div>
+            </div>
+            <div class="col-md-6">
+                <img src="images/menu.jpeg" alt="メニュー" style="width: 700px; height: auto;">
             </div>
         </div>
     </div>
