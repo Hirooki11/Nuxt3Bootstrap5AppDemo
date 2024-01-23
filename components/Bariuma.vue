dff85f5c9591f360996ce510e726c5a9dc175807
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-01-23 05:46:29 +0000

    ts

--- components/Bariuma.vue
+++ components/Bariuma.vue
@@ -1,20 +1,25 @@
 <template>
     <div class="Bariuma-container mb-5">
         <div class="row">
-            <div class="col-md-6">
-                <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;" class="mb-5">
-                <div class="information">
-                    <div>住所: 和歌山県岩出市川尻17-8</div>
-                    <div>電話番号: 0736-69-5850</div>
-                    <div>営業時間: 11:00～24:00(LO.23:30)※年末年始、連休時は店舗へお問い合わせください。</div>
-                    <div>駐車場: 32台</div>
-                    <div>席数: 44席</div>
+            <div class="col-md-7">
+                <div class="col-md-6">
+                    <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;" class="mb-5">
+                    <div class="information">
+                        <div>住所: 和歌山県岩出市川尻17-8</div>
+                        <div>電話番号: 0736-69-5850</div>
+                        <div>営業時間: 11:00～24:00(LO.23:30)※年末年始、連休時は店舗へお問い合わせください。</div>
+                        <div>駐車場: 32台</div>
+                        <div>席数: 44席</div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="images/menu.jpeg" alt="メニュー" style="width: 700px; height: auto;">
                 </div>
             </div>
-            <div class="col-md-6">
-                <img src="images/menu.jpeg" alt="メニュー" style="width: 700px; height: auto;">
-            </div>
         </div>
-
     </div>
 </template>
+
+<script setup lang="ts">
+const bariuma = useBariuma()
+</script>
