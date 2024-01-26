4c58b5a83e22e8c7e168eafc2d1fdc42eef9e765
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-01-26 06:46:11 +0000

    Atitle

--- components/Asahi.vue
+++ components/Asahi.vue
@@ -2,11 +2,11 @@
     <div class="Asahi container" style="font-size: 20px;">
         <div class="row">
             <div class="col-md-7">
-                <h2>本家第一旭本店</h2>
+                <h2>{{ ramen.Atitle }}</h2>
                 <img src="images/asahi.jpeg" alt="本家第一旭" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
-                <h3>本家第一旭本店</h3>
+                <h3>{{ ramen.Atitle }}</h3>
                 <p>〒600-8213</p>
                 <p>京都府京都市下京区東塩小路向畑町845</p>
                 <p>075-351-6321</p>
