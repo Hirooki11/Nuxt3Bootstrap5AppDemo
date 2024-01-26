fd40fb9987c26aaf9172778b4c59638021d28e7d
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-01-26 07:29:51 +0000

    Jtitle

--- components/Jinrai.vue
+++ components/Jinrai.vue
@@ -2,16 +2,12 @@
     <div class="Jinrai-container" style="font-size: 20px;">
         <div class="row">
             <div class="col-md-7">
-                <h2>神来</h2>
+                <h2>{{ ramen.Jtitle }}</h2>
                 <img src="images/ichiran.jpeg" alt="神来" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
-                <h3>熟成麺屋 神来 西院本店</h3>
-                <p>〒604-8855</p>
-                <p>京都府京都市中京区壬生淵田町1</p>
-                <p>075-315-9588</p>
-                <p>【月曜日〜木曜日】
-                    11:30〜24:00（L.O.23:30）</p>
+                <h3>{{ ramen.Jtitle2 }}</h3>
+                <p></p>
                 <p>【金曜日】
                     11:30〜24:30（L.O.24:00）</p>
                 <p>【土曜日】
@@ -22,3 +18,6 @@
         </div>
     </div>
 </template>
+<script setup lang="ts">
+const ramen = useRamen()
+</script>
