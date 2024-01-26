2867ebcfb6e15f8c111051d8ccec068d7f0a1f7c
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-01-26 06:58:03 +0000

    Ictitle

--- components/Ichiran.vue
+++ components/Ichiran.vue
@@ -2,11 +2,11 @@
     <div class="Ichiran-container" style="font-size: 20px;">
         <div class="row">
             <div class="col-md-7">
-                <h2>一蘭</h2>
+                <h2>{{ ramen.Ictitle }}</h2>
                 <img src="images/ichiran.jpeg" alt="一蘭" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
-                <h3>一蘭 京都烏丸店</h3>
+                <h3>{{ ramen.Ictitle2 }}</h3>
                 <p>〒600-8006</p>
                 <p>京都府京都市下京区立売中之町102-3 ミューズ389京都ビル2F</p>
                 <p>075-211-7755</p>
@@ -15,3 +15,6 @@
         </div>
     </div>
 </template>
+<script setup lang="ts">
+const ramen = useRamen()
+</script>
--- composables/ramen.ts
+++ composables/ramen.ts
@@ -1,4 +1,15 @@
 export const useRamen = () => useState('Ramen', () => {
     return {
         Atitle: "本家第一旭本店",
-    }})
+        Aadress: [
+            { text: "〒600-8213" },
+            { text: "京都府京都市下京区東塩小路向畑町845" },
+            { text: "075-351-6321" },
+            { text: "600～25:00" },
+            { text: "席数: 44席" },
+        ],
+        Ictitle: "一蘭",
+        Ictitle2: "一蘭 京都烏丸店",
+    }
+})
+       
