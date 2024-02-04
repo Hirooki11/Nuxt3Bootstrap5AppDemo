1664f50a05fff66571097752b2649387eb340c94
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-02-04 05:44:47 +0000

    KKtitle

--- components/Kakumenya.vue
+++ components/Kakumenya.vue
@@ -2,7 +2,7 @@
     <div class="Kakumenya-container" style="font-size: 20px;">
         <div class="row">
             <div class="col-md-7">
-                <h2>革麺家</h2>
+                <h2>{{ ramen.Kktitle }}</h2>
                 <img src="images/kakumenya.jpeg" alt="革麺家" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
@@ -17,5 +17,5 @@
 </template>
 
 <script setup lang="ts">
-const bariuma = useBariuma()
+const ramen = useRamen()
 </script>
