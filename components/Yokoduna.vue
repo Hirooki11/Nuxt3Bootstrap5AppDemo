e01b4cd483dc3d6964a9ae3a0408f3cb540f13c5
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-02-05 08:20:43 +0000

    Yk

--- components/Yokoduna.vue
+++ components/Yokoduna.vue
@@ -2,16 +2,19 @@
     <div class="Yokoduna-container" style="font-size: 20px;">
         <div class="row">
             <div class="col-md-7">
-                <h2>ラーメン横綱</h2>
+                <h2>{{ ramen.Yktitle }}</h2>
                 <img src="images/yokoduna.jpeg" alt="横綱" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
-                <h3>ラーメン横綱 五条店</h3>
-                <p>〒600-8375</p>
-                <p>京都市下京区大宮通り五条西入る</p>
-                <p>075-361-6050</p>
-                <p>午前11:00～翌朝5:00 (ラストオーダー 5:00)</p>
+                <h3>{{ ramen.Yktitle2 }}</h3>
+                <div v-for="Yk in ramen.Ykadress">
+                    <p>{{ Yk.text }}</p>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+const ramen = useRamen()
+</script>
