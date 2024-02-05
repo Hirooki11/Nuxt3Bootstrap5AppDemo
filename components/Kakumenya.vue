3a1d317385ec64120d3d0f61a909473127d8f93c
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-02-05 07:58:55 +0000

    かくめんや

--- components/Kakumenya.vue
+++ components/Kakumenya.vue
@@ -6,11 +6,10 @@
                 <img src="images/kakumenya.jpeg" alt="革麺家" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
-                <h3>横浜家系ラーメン 革麺家 岩出店</h3>
-                <p>〒649-6235</p>
-                <p>和歌山県岩出市西野211-1 1F</p>
-                <p>0736-67-7030</p>
-                <p>11:00～24:30(L.O.24:00)</p>
+                <h3>{{  ramen.Kktitle2 }}</h3>
+                <div v-for="Kk in ramen.Kkadress">
+                    <p>{{ Kk.text }}</p>
+                </div>
             </div>
         </div>
     </div>
