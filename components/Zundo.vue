30ca8936b8a356cbcd995741ec6f94102dc9f5d4
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-02-05 08:26:28 +0000

    ずんどう

--- components/Zundo.vue
+++ components/Zundo.vue
@@ -2,17 +2,19 @@
     <div class="Zundo-container" style="font-size: 20px;">
         <div class="row">
             <div class="col-md-7">
-                <h2>ずんどう屋</h2>
+                <h2>{{ ramen.Zntitle }}</h2>
                 <img src="images/zundo.png" alt="ずんどう屋" style="width: 700px; height: auto;">
             </div>
             <div class="col-md-5">
-                <h3>ずんどう屋 四条烏丸店</h3>
-                <p>〒604-8132</p>
-                <p>京都府京都市中京区西魚屋町619</p>
-                <p>075-746-4808</p>
-                <p>＜通常＞ 11:00～22:00</p>
-                <p>(金土祝前日11:00~24:00)</p>
+                <h3>{{ ramen.Zntitle2 }}</h3>
+                <div v-for="Zn in ramen.Znadress">
+                    <p>{{ Zn.text }}</p>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+const ramen = useRamen()
+</script>
