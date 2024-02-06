44f52a9b012bd4583849c49bd1c51ca9b330334e
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-02-06 05:28:53 +0000

    一蘭

--- components/Home.vue
+++ components/Home.vue
@@ -1,12 +1,19 @@
 <template>
-<div class="home-container">
-      
-   <h2>この記事では<span class="text-danger fw-bold">ラーメン</span>が大好きな私が今まで食べてきたラーメンを紹介しようと思います</h2>
-  <div class="barikoku">
-    <img src="images/bariuma1.jpg" alt="ラーメン" style="width: 700px; height: auto;">
+  <div class="home-container">
+
+    <h2>この記事では<span class="text-danger fw-bold">ラーメン</span>が大好きな私が今まで食べてきたラーメンを紹介しようと思います</h2>
+    <div class="barikoku">
+      <div class="row">
+        <div class="col-md-6">
+          <img src="images/bariuma1.jpg" alt="ラーメン" style="width:500px; height: auto;">
+        </div>
+        <div class="col-md-6">
+          <img src="images/ichiran_s.png" alt="一蘭" style="width:500px; height: auto;">
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-  </template>
+</template>
     
-  <script>
-  </script>
+<script>
+</script>
