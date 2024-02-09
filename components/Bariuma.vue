30ee5d869ce3217cdd47da0af5d19d6fa25c734a
Hirooki11 <144088393+Hirooki11@users.noreply.github.com>
2024-02-09 10:44:02 +0000

    img

--- components/Bariuma.vue
+++ components/Bariuma.vue
@@ -2,13 +2,13 @@
     <div class="Bariuma-container mb-5">
         <div class="row">
             <div class="col-md-6 mb-5">
-                <img src="images/bariuma_f.jpeg" alt="外観" style="width: 700px; height: auto;">
+                <img src="images/bariuma_f.jpeg" class="img-thumbnail" alt="外観">
                 <div v-for="ba in bariuma.adress">
                     <p>{{ ba.text }}</p>
                 </div>
             </div>
             <div class="col-md-6">
-                <img src="images/menu.jpeg" alt="メニュー" style="width: 700px; height: auto;">
+                <img src="images/menu.jpeg" class="img-thumbnail" alt="メニュー">
             </div>
         </div>
     </div>
